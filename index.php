8456bf2e33fa3d5e4d382fa4f689622d5ebb980b
Кадыр Атаханов <kadyr.atakhanov@gmail.com>
2023-09-09 20:15:06 +0300

    1

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
+error_reporting(error_reporting() & ~E_NOTICE);
+date_default_timezone_set('Asia/Calcutta');
+mb_internal_encoding("UTF-8");
 phpinfo();
-// error_reporting(error_reporting() & ~E_NOTICE);
-// date_default_timezone_set('Asia/Calcutta');
 
-// mb_internal_encoding("UTF-8");
 
 
 // $_root_path = $_SERVER["DOCUMENT_ROOT"].'/';
