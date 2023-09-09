847124d1d56e7a7c13b7ae7fbf89c4f10203f95f
Кадыр Атаханов <kadyr.atakhanov@gmail.com>
2023-09-09 20:15:42 +0300

    1

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-error_reporting(error_reporting() & ~E_NOTICE);
+// error_reporting(error_reporting() & ~E_NOTICE);
 date_default_timezone_set('Asia/Calcutta');
 mb_internal_encoding("UTF-8");
 phpinfo();
